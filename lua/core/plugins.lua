93332e46019f2078c76a8266b8b481d2bb80910d
kesleymartins <kesley.martins@vivaldi.net>
2023-04-27 02:25:36 -0300

    add bufferline plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -30,6 +30,12 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  
+  use {
+    'akinsho/bufferline.nvim', 
+    tag = "*", 
+    requires = 'nvim-tree/nvim-web-devicons'
+  }
 
   use("norcalli/nvim-colorizer.lua")
   use("lewis6991/gitsigns.nvim")
