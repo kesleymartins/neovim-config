2f2ac7da2064c9d86383f54eba34bb17fb1b7c32
kesleymartins <kesley.martins@vivaldi.net>
2023-04-27 02:37:15 -0300

    add neotree config

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,3 +1,4 @@
+require('core.plugin_config.neotree')
 require('core.plugin_config.catppuccin')
 require('core.plugin_config.lualine')
 require('core.plugin_config.colorizer')
---
+++ lua/core/plugin_config/neotree.lua
@@ -0,0 +1,8 @@
+require('neo-tree').setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false
+    }
+  }
+})
