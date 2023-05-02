066659aa10b918a35c64364e98e8a8b4748d547b
kesleymartins <kesley.martins@vivaldi.net>
2023-05-01 22:41:56 -0300

    add new keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,10 @@
 local map = vim.api.nvim_set_keymap
-local kmap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-map('n', '<C-b>', ':Neotree focus<CR>', opts)
-map('n', '<C-q>', ':bd<CR>', opts)
+-- Normal mode
+map('n', '<C-b>', ':Neotree left focus<CR>', opts)  -- Focus Neotree
+map('n', '<C-S-Down>', ':move +1<CR>', opts)        -- Move line Down
+map('n', '<C-S-Up>', ':move -2<CR>', opts)          -- Move line Up
+map('n', '<C-S-Right>', ':bnext<CR>', opts)         -- Next Buffer
+map('n', '<C-S-Left>', ':bprevious<CR>', opts)      -- Previous Buffer
+map('n', '<C-q>', ':bd<CR>', opts)                  -- Close Buffer
