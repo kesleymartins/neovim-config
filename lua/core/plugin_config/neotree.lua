ca3f05b26ef87758aa2c530a501ab00972247acc
kesleymartins <kesley.martins@vivaldi.net>
2023-05-02 01:07:29 -0300

    update neotree config

--- lua/core/plugin_config/neotree.lua
+++ lua/core/plugin_config/neotree.lua
@@ -1,8 +1,28 @@
 require('neo-tree').setup({
+  default_component_configs = {
+    icon = {
+      folder_empty = "-"
+    },
+    git_status = {
+      symbols = {
+        added = "",
+        modified = "[M]",
+        deleted = "",
+        renamed = "",
+        untracked = "[U]",
+        ignored = "",
+        unstaged = "",
+        staged = "[S]",
+        conflit = "[!]"
+      }
+    }
+  },
   filesystem = {
     filtered_items = {
       hide_dotfiles = false,
-      hide_gitignored = false
+      hide_by_name = {
+        ".git"
+      }
     }
   }
 })
