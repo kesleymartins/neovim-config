d1c5a90cbe9744be168e804b0697a8081a3a2726
kesleymartins <kesley.martins@vivaldi.net>
2023-05-05 02:14:54 -0300

    update Neotree keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,12 +2,15 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Normal Mode
-map('n', '<C-b>', ':Neotree right focus<CR>', opts)   -- Focus Neotree
-map('n', '<C-q>', ':bd<CR>', opts)                    -- Close Buffer
-map('n', '<C-Up>', ':move -2<CR>', opts)              -- Move line Up
-map('n', '<C-Down>', ':move +1<CR>', opts)            -- Move line Down
-map('n', '<C-S-Right>', ':bnext<CR>', opts)           -- Next Buffer
-map('n', '<C-S-Left>', ':bprevious<CR>', opts)        -- Previous Buffer
+map('n', '<Space>nf', ':Neotree right focus<CR>', opts)   -- Focus Neotree
+map('n', '<Space>nt', ':Neotree right toggle<CR>', opts)  -- Toggle Neotree
+map('n', '<Space>nc', ':Neotree close<CR>', opts)         -- Close Neotree 
+map('n', '<C-q>', ':bd<CR>', opts)                        -- Close Buffer
+map('n', '<C-Up>', ':move -2<CR>', opts)                  -- Move line Up
+map('n', '<C-Down>', ':move +1<CR>', opts)                -- Move line Down
+map('n', '<C-S-Right>', ':bnext<CR>', opts)               -- Next Buffer
+map('n', '<C-S-Left>', ':bprevious<CR>', opts)            -- Previous Buffer
+
 
 -- Visual Mode Only
 map('x', '<C-Up>', ":move '<-2<CR>gv", opts)    -- Move line(s) Up
