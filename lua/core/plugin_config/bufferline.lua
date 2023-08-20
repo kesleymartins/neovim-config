3cd66d931b4b2f0fd8eca370e390ba5ea7b009c8
kesleymartins <kesley.martins@vivaldi.net>
2023-08-20 14:37:49 -0300

    update bufferline config

--- lua/core/plugin_config/bufferline.lua
+++ lua/core/plugin_config/bufferline.lua
@@ -1,1 +1,9 @@
-require('bufferline').setup()
+require('bufferline').setup({
+  options = {
+    hover = {
+      enabled = true,
+      reveal = {'close'}
+    },
+    separator_style = "slant" -- slant slope thick thin
+  }
+})
