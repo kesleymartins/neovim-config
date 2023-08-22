374c3556d0b5bdcc1ef5f471f9b266f7d0376766
kesleymartins <kesley.martins@vivaldi.net>
2023-08-22 14:38:22 -0300

    update file identation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,12 +2,16 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Normal Mode
+
+map('n', '<Space>nf', ':NvimTreeFocus<CR>', opts)         -- Set focus on NvimTree
+map('n', '<Space>nc', ':NvimTreeClose<CR>', opts)         -- Close NvimTree
+map('n', '<Space>nr', ':NvimTreeRefresh<CR>', opts)       -- Refresh NvimTree
 map('n', '<C-q>', ':bd<CR>', opts)                        -- Close Buffer
 map('n', '<A-Up>', ':move -2<CR>', opts)                  -- Move line Up
 map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
 map('n', '<C-S-Right>', ':bnext<CR>', opts)               -- Next Buffer
 map('n', '<C-S-Left>', ':bprevious<CR>', opts)            -- Previous Buffer
-map('n', '<Space>ff', ':Telescope find_files<CR>', opts)      -- Find files with Telescope
+map('n', '<Space>ff', ':Telescope find_files<CR>', opts)  -- Find files with Telescope
 
 -- Visual Mode Only
 map('x', '<A-Up>', ":move '<-2<CR>gv", opts)    -- Move line(s) Up
--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,10 +1,11 @@
+local g = vim.g
 local o = vim.opt
 local cmd = vim.cmd
 
 -- Options
 o.number = true           -- Add line numbers
 o.relativenumber = true   -- Set relative line numbers
-o.cursorline = true	  -- Add cursor line
+o.cursorline = true	      -- Add cursor line
 o.swapfile = false    	  -- impede a criacao de arquivos .swp 
 
 -- Tab settings
@@ -15,3 +16,8 @@ o.tabstop = 2
 
 -- Commands
 cmd.colorscheme('catppuccin-macchiato')
+
+-- Global
+-- Config for NvimTree
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
