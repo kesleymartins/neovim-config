77ea055988d94d842e4857cbcf95cdafa727bbbf
kesleymartins <kesley.martins@vivaldi.net>
2023-08-22 14:03:35 -0300

    add keymap to open telescope file finder

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,14 +2,15 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Normal Mode
-map('n', '<Space>nf', ':Neotree right focus<CR>', opts)   -- Focus Neotree
-map('n', '<Space>nt', ':Neotree right toggle<CR>', opts)  -- Toggle Neotree
+map('n', '<Space>nf', ':Neotree focus<CR>', opts)   -- Focus Neotree
+map('n', '<Space>nt', ':Neotree toggle<CR>', opts)  -- Toggle Neotree
 map('n', '<Space>nc', ':Neotree close<CR>', opts)         -- Close Neotree 
 map('n', '<C-q>', ':bd<CR>', opts)                        -- Close Buffer
 map('n', '<A-Up>', ':move -2<CR>', opts)                  -- Move line Up
 map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
 map('n', '<C-S-Right>', ':bnext<CR>', opts)               -- Next Buffer
 map('n', '<C-S-Left>', ':bprevious<CR>', opts)            -- Previous Buffer
+map('n', '<Space>ff', ':Telescope find_files<CR>', opts)      -- Find files with Telescope
 
 -- Visual Mode Only
 map('x', '<A-Up>', ":move '<-2<CR>gv", opts)    -- Move line(s) Up
