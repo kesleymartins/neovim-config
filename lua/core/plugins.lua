785ef11147330925a7ec5a01d17a991f492f2293
kesleymartins <kesley.martins@vivaldi.net>
2023-08-22 14:02:15 -0300

    fix identation

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -47,8 +47,8 @@ return require('packer').startup(function(use)
   -- Neotree
   use({
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  requires = {
+    branch = "v2.x",
+    requires = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
