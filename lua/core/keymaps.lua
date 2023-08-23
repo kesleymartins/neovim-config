537dbb56c06575a69a903b9d3823c24f9ac458da
kesleymartins <kesley.martins@vivaldi.net>
2023-08-23 00:37:26 -0300

    remove buffer switcher keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,7 +2,6 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Normal Mode
-
 map('n', '<Space>nf', ':NvimTreeFocus<CR>', opts)         -- Set focus on NvimTree
 map('n', '<Space>nc', ':NvimTreeClose<CR>', opts)         -- Close NvimTree
 map('n', '<Space>nr', ':NvimTreeRefresh<CR>', opts)       -- Refresh NvimTree
@@ -11,8 +10,6 @@ map('n', '<Space>fg', ':Telescope live_grep<CR>', opts)   -- Telescope live_grep
 map('n', '<C-q>', ':bd<CR>', opts)                        -- Close Buffer
 map('n', '<A-Up>', ':move -2<CR>', opts)                  -- Move line Up
 map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
-map('n', '<C-S-Right>', ':bnext<CR>', opts)               -- Next Buffer
-map('n', '<C-S-Left>', ':bprevious<CR>', opts)            -- Previous Buffer
 
 -- Visual Mode Only
 map('x', '<A-Up>', ":move '<-2<CR>gv", opts)    -- Move line(s) Up
