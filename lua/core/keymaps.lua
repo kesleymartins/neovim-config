b96259544195f605679e87265290ca6a9075e855
kesleymartins <kesley.martins@vivaldi.net>
2023-09-05 18:24:19 -0300

    move nvim keymaps to own section

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,8 +2,6 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Normal Mode
-map('n', '<Space>nf', ':NvimTreeFocus<CR>', opts)         -- Focus on Nvim Tree
-map('n', '<Space>nc', ':NvimTreeClose<CR>', opts)         -- Close Nvim Tree
 map('n', '<A-Up>', ':move -2<CR>', opts)                  -- Move line Up
 map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
 
@@ -15,3 +13,7 @@ map('x', '<A-Down>', ":move '>+1<CR>gv", opts)            -- Move line(s) Down
 local telescope = require('telescope.builtin')
 map('n', '<Space>ff', telescope.find_files, opts)         -- Open Telescope find files page
 map('n', '<Space>fg', telescope.live_grep, opts)          -- Open Telescope live grep page
+
+-- Nvim Tree
+map('n', '<Space>nc', ':NvimTreeClose<CR>', opts)         -- Close Nvim Tree
+map('n', '<Space>nf', ':NvimTreeFocus<CR>', opts)         -- Focus on Nvim Tree
