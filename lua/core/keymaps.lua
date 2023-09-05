afdd95268e238c0f5c7b00e421b79c62d92e5c2f
kesleymartins <kesley.martins@vivaldi.net>
2023-09-05 18:23:32 -0300

    align keymap description

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,5 +8,5 @@ map('n', '<A-Up>', ':move -2<CR>', opts)                  -- Move line Up
 map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
 
 -- Visual Mode Only
-map('x', '<A-Up>', ":move '<-2<CR>gv", opts)    -- Move line(s) Up
-map('x', '<A-Down>', ":move '>+1<CR>gv", opts)  -- Move line(s) Down
+map('x', '<A-Up>', ":move '<-2<CR>gv", opts)              -- Move line(s) Up
+map('x', '<A-Down>', ":move '>+1<CR>gv", opts)            -- Move line(s) Down
