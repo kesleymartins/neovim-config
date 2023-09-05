a3f794243a7faf03d205d0e3a86114bfabb0e124
kesleymartins <kesley.martins@vivaldi.net>
2023-09-05 18:29:50 -0300

    add colorizer plugin

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,3 +1,4 @@
 require('core.plugin_config.lazy')
 require('core.plugin_config.nvim-tree')
 require('core.plugin_config.telescope')
+require('core.plugin_config.colorizer')
--- lua/core/plugin_config/lazy.lua
+++ lua/core/plugin_config/lazy.lua
@@ -1,6 +1,7 @@
 local plugins = {
   "nvim-tree/nvim-tree.lua",
   "wakatime/vim-wakatime",
+  "norcalli/nvim-colorizer.lua",
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     dependencies = { 'nvim-lua/plenary.nvim' }
