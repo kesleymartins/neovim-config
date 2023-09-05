6b82782e01c56f16991a31638f3a936461d8de8d
kesleymartins <kesley.martins@vivaldi.net>
2023-09-05 18:23:58 -0300

    add telescope keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,3 +10,8 @@ map('n', '<A-Down>', ':move +1<CR>', opts)                -- Move line Down
 -- Visual Mode Only
 map('x', '<A-Up>', ":move '<-2<CR>gv", opts)              -- Move line(s) Up
 map('x', '<A-Down>', ":move '>+1<CR>gv", opts)            -- Move line(s) Down
+
+-- Telescope
+local telescope = require('telescope.builtin')
+map('n', '<Space>ff', telescope.find_files, opts)         -- Open Telescope find files page
+map('n', '<Space>fg', telescope.live_grep, opts)          -- Open Telescope live grep page
