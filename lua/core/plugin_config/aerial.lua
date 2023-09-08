a9538dda78969c55833ecf2d93d89115b6b4951d
kesleymartins <kesley.martins@vivaldi.net>
2023-09-08 01:56:50 -0300

    add move keymaps to aerial setup

--- lua/core/plugin_config/aerial.lua
+++ lua/core/plugin_config/aerial.lua
@@ -1,2 +1,8 @@
-require('aerial').setup()
+local map = vim.keymap.set
 
+require('aerial').setup({
+  on_attach = function(bufnr)
+    map('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
+    map('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
+  end
+})
