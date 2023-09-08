249f387bd3d6c9101dc0e37d193214b2e6f17389
kesleymartins <kesley.martins@vivaldi.net>
2023-09-08 00:39:42 -0300

    add lsp server capabilities

--- lua/core/plugin_config/lspconfig.lua
+++ lua/core/plugin_config/lspconfig.lua
@@ -1,13 +1,16 @@
 require('mason').setup()
 
 local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local mason_lsp = require('mason-lspconfig')
 mason_lsp.setup()
 
 mason_lsp.setup_handlers({
   function(server_name)
-    lspconfig[server_name].setup({})
+    lspconfig[server_name].setup({
+      capabilities = capabilities
+    })
   end
 })
 
