8a853b876c9d3eb0c7765cdc27faed246a6f90b9
kesleymartins <kesley.martins@vivaldi.net>
2023-09-16 12:39:42 -0300

    remove white spaces

--- lua/core/plugin_config/lazy.lua
+++ lua/core/plugin_config/lazy.lua
@@ -9,8 +9,8 @@ local plugins = {
   { "lukas-reineke/indent-blankline.nvim" },
   { "nvim-treesitter/nvim-treesitter" },
   { "morhetz/gruvbox" },
-  { 
-    "kesleymartins/monoke.nvim",  dev = false 
+  {
+    "kesleymartins/monoke.nvim",  dev = false
   },
   {
     "williamboman/mason.nvim",
