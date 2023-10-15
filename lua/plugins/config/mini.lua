bce905936295a6026321a0ef0d5fe2cff04b0646
kesleymartins <kesley.martins@vivaldi.net>
2023-10-15 02:19:25 -0300

    enable pairs mini module

--- lua/plugins/config/mini.lua
+++ lua/plugins/config/mini.lua
@@ -2,6 +2,7 @@ require('mini.animate').setup()
 require('mini.comment').setup()
 require('mini.cursorword').setup()
 require('mini.move').setup()
+require('mini.pairs').setup()
 
 require('mini.indentscope').setup({
   symbol = '│'
