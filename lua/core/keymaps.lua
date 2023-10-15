7cc9a0637f4472c0d9db2c661b03b581242e8ede
kesleymartins <kesley.martins@vivaldi.net>
2023-10-15 02:14:25 -0300

    remove line movement keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,14 +7,6 @@ map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
 
--- Move lines
-map('n', '<A-k>', ':move -2<CR>', opts)
-map('n', '<A-j>', ':move +1<CR>', opts)
-
-map('x', '<A-k>', ":move '<-2<CR>gv", opts)
-map('x', '<A-j>', ":move '>+1<CR>gv", opts)
-
-
 -- Use S-u for redo 
 map('n', 'U', '<C-r>', opts)
 
