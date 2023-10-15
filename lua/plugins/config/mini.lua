2aa4b61fa90af580a145e468784cf312d61d88c0
kesleymartins <kesley.martins@vivaldi.net>
2023-10-15 02:16:05 -0300

    enable mini move module

--- lua/plugins/config/mini.lua
+++ lua/plugins/config/mini.lua
@@ -1,6 +1,7 @@
 require('mini.animate').setup()
 require('mini.comment').setup()
 require('mini.cursorword').setup()
+require('mini.move').setup()
 
 require('mini.indentscope').setup({
   symbol = '│'
