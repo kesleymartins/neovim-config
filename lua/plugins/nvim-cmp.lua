19811536d377d2b34d47ab6eab681102bea601bd
kesleymartins <kesley.martins@vivaldi.net>
2023-10-21 14:41:03 -0300

    setup nvim-cmp

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,59 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+    "dcampos/nvim-snippy",
+    "dcampos/cmp-snippy",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("snippy").expand_snippet(args.body)
+        end,
+      },
+      window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "snippy" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    cmp.setup.filetype("gitcommit", {
+      sources = cmp.config.sources({
+        { name = "git" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+  end,
+}
