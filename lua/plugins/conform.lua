b5906638f6f5433bcb2100d8f0a35b932bbc93b6
kesleymartins <kesley.martins@vivaldi.net>
2024-01-24 00:39:50 -0300

    setup conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
